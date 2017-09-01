f56f8e546d880fd23497c0fbba07a043cd4776d3
carotene512 <karoten512@gmail.com>
2017-09-01 10:59:00 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  private deliverProperty: string = '子供に送るよ。';
+  private notDeliverProperty: string = '子供に送らないよ。';
 }
